c8c27ba9981d7cab1878e87c83e468cc2f03c1f3
Zachary Mowatt <zamowatt@gmail.com>
2025-05-23 20:03:01 -0400

    Completing phase 0 js functions lab

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+function calculateTax(amount){
+    let taxValue = (amount * .1)
+    return taxValue
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase()
+}
 
+function findMaximum (num1, num2){
+    if (num1 > num2){
+        return num1
+    } else {
+        return num2
+    }
+}
 
+function isPalindrome (word) {
+    const reversedWord = word.split("").reverse().join("")
+    return reversedWord === word
+}
+
+function calculateDiscountedPrice (originalPrice, discountPercentage){
+    const discount = (originalPrice * (discountPercentage/100))
+
+    const newPrice = originalPrice - discount
+
+    return newPrice
+
+}
 
 
 // This is required for the test to function properly  
